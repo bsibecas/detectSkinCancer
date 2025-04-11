8dace9df4fc5ef0955414ecc8617f2b18b97bb4b
Blanca <b.sibecas@gmail.com>
2025-04-11 20:10:11 +0200

    [FIX] changed the rate to 0.0001

--- detect-skin-cancer.py
+++ detect-skin-cancer.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     model = model.to('cuda')
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
     train_loss = []
     train_accuracy = []
