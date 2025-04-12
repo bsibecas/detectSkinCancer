3330efe2858eefc8e9b15e5b6c6080409b5842ff
Blanca <b.sibecas@gmail.com>
2025-04-12 12:56:35 +0200

    [ADD] add a dropout

--- detect-skin-cancer.py
+++ detect-skin-cancer.py
@@ -35,7 +35,13 @@ if __name__ == '__main__':
     # Modelo y configuración
     model = torchvision.models.resnet18(pretrained=True)
 
-    model.fc = torch.nn.Linear(model.fc.in_features, 2)
+    #model.fc = torch.nn.Linear(model.fc.in_features, 2)
+    model.fc = torch.nn.Sequential(
+        torch.nn.Linear(model.fc.in_features, 512),  # Capa FC adicional
+        torch.nn.ReLU(),  # Función de activación ReLU
+        torch.nn.Dropout(p=0.25),  # Dropout con 25% de probabilidad (puedes cambiar el valor)
+        torch.nn.Linear(512, 2)  # Capa final de salida
+    )
     model = model.to('cuda')
 
     criterion = torch.nn.CrossEntropyLoss()
